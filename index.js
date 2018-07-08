5ce5515dab818e70694aea1f10556a8a2952283b
koho20002000@yahoo.com Berryman <koho20002000@yahoo.com>
2018-07-08 04:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var Number
-funtction; add(a,b){
-  number= a+b;
+var number;
+funtction add(a,b){
+  number= a + b;
+  return number;
+}
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
   return number;
 }
